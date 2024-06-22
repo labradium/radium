014215bafd2a21e6310df09a448600d9c09130c0
thevgseven <ivgseven@gmail.com>
2024-06-22 23:14:07 +0530

    feat: new templates added

---
+++ next/next-ecom/app/page.tsx
@@ -0,0 +1,23 @@
+import { ThemeToggle } from "@/ui/components/theme-toggle";
+import { Button } from "@/ui/primitives/button";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
+export default function Home() {
+  return (
+    <main
+      className={"relative flex h-screen flex-col items-center justify-center"}
+    >
+      <h1 className={"text-4xl font-semibold"}>Radium Next.js Template</h1>
+      <a
+        href={"https://github.com/vgseven/radium"}
+        className={"mb-6"}
+        target={"_blank"}
+      >
+        <Button className={"mt-6 rounded-full"}>
+          <GitHubLogoIcon className={"mx-2"} /> Star on GitHub
+        </Button>
+      </a>
+      <ThemeToggle />
+    </main>
+  );
+}
