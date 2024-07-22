97bc7f693b18f39febd157fb27e3112a178f35ba
thevgseven <ivgseven@gmail.com>
2024-07-22 17:07:24 +0530

    core: react-essential added

--- upcomings/react-essential/vite.config.js
+++ upcomings/react-essential/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import { resolve } from "node:path";
-
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
   resolve: {
