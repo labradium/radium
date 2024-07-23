5fb0cdd64a9f721ee8fad6a469061a2ae4f6f472
thevgseven <ivgseven@gmail.com>
2024-07-23 16:51:55 +0530

    chore: readme update

--- upcoming/react-essential/vite.config.js
+++ upcoming/react-essential/vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import { resolve } from "node:path";
 export default defineConfig({
-    plugins: [react(), TanStackRouterVite()],
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "./src"),
-        },
+  plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
     },
+  },
 });
